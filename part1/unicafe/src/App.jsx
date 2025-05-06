54546e1e224028cf6578d4ca3c8bc59882b9f4d7
Miguel Ines <miguel.joao.ines@gmail.com>
2025-05-06 20:03:25 +0100

    1.11: unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,7 @@ const Statistics = (props) => {
   const { good, neutral, bad } = props
   const total = good + neutral + bad
   const average = (good - bad) / total
-  const positive = (good / total) * 100
+  const positive = ((good / total) * 100) + ' %'
   if (total === 0) {
     return (
       <>
@@ -20,21 +20,26 @@ const Statistics = (props) => {
   return (
     <>
       <h1>statistics</h1>
-      <StatisticsLine text="good" value={good} />
-      <StatisticsLine text="neutral" value={neutral} />
-      <StatisticsLine text="bad" value={bad} />
-      <StatisticsLine text="all" value={total} />
-      <StatisticsLine text="average" value={average} />
-      <StatisticsLine text="positive" value={positive} />
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={total} />
+          <StatisticsLine text="average" value={average} />
+          <StatisticsLine text="positive" value={positive} />
+        </tbody>
+      </table>
     </>
   )
 }
 
 const StatisticsLine = (props) => {
   return (
-    <>
-      <p>{props.text} {props.value}</p>
-    </>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -49,14 +54,14 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
+    <>
       <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
       <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
+    </>
   )
 }
 
