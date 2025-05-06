a26024a3e91febd3bedbb5f89fd0989be2335039
Miguel Ines <miguel.joao.ines@gmail.com>
2025-05-06 19:43:46 +0100

    1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,16 +9,24 @@ const Statistics = (props) => {
   const total = good + neutral + bad
   const average = (good - bad) / total
   const positive = (good / total) * 100
+  if (total === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    )
+  }
   return (
-  <>
-    <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {total}</p>
-    <p>average {average}</p>
-    <p>positive {positive} %</p>
-  </>
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </>
   )
 }
 const App = () => {
