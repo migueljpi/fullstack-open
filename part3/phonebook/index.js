3bffb4fea7fe4a79c91469e9c34a120cb12de790
Miguel Ines <miguel.joao.ines@gmail.com>
2025-05-13 20:04:03 +0100

    3.8*: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 const morgan = require('morgan');
-app.use(morgan('tiny'));
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : '';
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
