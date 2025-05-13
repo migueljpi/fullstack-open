fd071514f903a8a07a44f82762708fd53300a6bf
Miguel Ines <miguel.joao.ines@gmail.com>
2025-05-13 19:38:55 +0100

    3.5: Phonebook backend step 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
 
 let persons = [
   { id: "1", name: "Arto Hellas", number: "040-123456" },
@@ -34,6 +35,17 @@ app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id;
   persons = persons.filter(person => person.id !== id);
   response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+  const person = {
+    id: Math.floor(Math.random() * 100).toString(),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
