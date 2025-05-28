2ec62741e55fee0bef07a69d2431c8020adc42d9
Miguel Ines <miguel.joao.ines@gmail.com>
2025-05-28 16:33:47 +0100

    3.18*: Phonebook database step 6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -41,16 +41,18 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get ('/info', (request, response) => {
+app.get('/info', (request, response) => {
   const date = new Date();
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>
-  `);
+  Person.countDocuments({}).then(count => {
+    response.send(`
+      <p>Phonebook has info for ${count} people</p>
+      <p>${date}</p>
+    `);
+  });
 });
 
 
-app.get('/api/notes/:id', (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
