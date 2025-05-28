0ca756472d891701cef447b1758d0b549e8a0800
Miguel Ines <miguel.joao.ines@gmail.com>
2025-05-28 15:17:48 +0100

    3.15: Phonebook database, step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
 const express = require('express');
+const Person = require('./models/person')
+
 const app = express();
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(requestLogger)
+
 const morgan = require('morgan');
 // const cors = require('cors')
 // app.use(cors())
-app.use(express.static('dist'))
-const Person = require('./models/person')
-
-const password = process.argv[2];
-
-// const Entry = mongoose.model('Entry', entrySchema);
 
 
 morgan.token('body', (req) => {
@@ -47,11 +55,13 @@ app.get('/api/persons/:id', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter(person => person.id !== id);
-  response.status(204).end();
-});
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
@@ -77,6 +87,25 @@ app.post('/api/persons', (request, response) => {
     response.status(500).json({ error: 'Database error' });
   });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
