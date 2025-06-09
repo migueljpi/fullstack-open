3f4d25980184e01664dc78fe7fa1c7e705cfbf8f
Miguel Ines <miguel.joao.ines@gmail.com>
2025-06-09 14:25:11 +0100

    4.19: Blog List Expansion, step 7

--- part4/blog_list/utils/middleware.js
+++ part4/blog_list/utils/middleware.js
@@ -19,6 +19,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'MongoServerError' && error.code === 11000) {
     return response.status(400).json({ error: 'username must be unique' })
+    } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
   next(error)
