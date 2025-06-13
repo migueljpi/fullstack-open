bccd46e98924c310ab41b055af13b61c329531d7
Miguel Ines <miguel.joao.ines@gmail.com>
2025-06-13 14:56:45 +0100

    5.2: Blog List Frontend, step 2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -15,6 +15,15 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+  const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+  if (loggedUserJSON) {
+    const user = JSON.parse(loggedUserJSON)
+    setUser(user)
+    blogService.setToken(user.token)
+  }
+}, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -23,7 +32,7 @@ const App = () => {
         username, password,
       })
       window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
+        'loggedBlogappUser', JSON.stringify(user)
       )
       blogService.setToken(user.token)
       setUser(user)
@@ -32,6 +41,12 @@ const App = () => {
     } catch (exception) {
       console.error('Login failed:', exception)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+    blogService.setToken(null)
   }
 
   const loginForm = () => (
@@ -70,8 +85,8 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
-      {/* <p>token: {user.token}</p> */}
+      <p>{user.name} logged in
+      <button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
