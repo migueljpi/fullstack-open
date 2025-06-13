826a55d03b599a730cbc4ded27fa33ee926bf8ce
Miguel Ines <miguel.joao.ines@gmail.com>
2025-06-13 18:31:09 +0100

    5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? 'hide' : 'view'}
+        </button>
+      </div>
+      {showDetails
+        ? (
+          <div>
+            <div>Title: {blog.title}</div>
+            <div>URL: {blog.url}</div>
+            <div>Likes: {blog.likes} <button>like</button></div>
+            <div>Author: {blog.author}</div>
+          </div>
+        )
+        : null
+      }
+    </div>
+  )
+}
 
 export default Blog
