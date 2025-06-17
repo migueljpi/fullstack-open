46f48b11b80544d683e1abcbabdb3b7063ddb1f4
Miguel Ines <miguel.joao.ines@gmail.com>
2025-06-17 17:33:12 +0100

    5.18: Blog List End To End Testing, step 2 - front end

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -79,6 +79,7 @@ const App = () => {
       <div>
         username
         <input
+          data-testid="username"
           type="text"
           value={username}
           name="Username"
@@ -88,6 +89,7 @@ const App = () => {
       <div>
         password
         <input
+          data-testid="password"
           type="password"
           value={password}
           name="Password"
