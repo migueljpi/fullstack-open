95799dfc7f9adcdda9a135cadd24bc827c757bdf
Miguel Ines <miguel.joao.ines@gmail.com>
2025-06-17 17:32:12 +0100

    5.18: Blog List End To End Testing, step 2

--- part5/e2e_test/tests/blog_app.spec.js
+++ part5/e2e_test/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
-const { test, expect, describe, beforeEach } = require('@playwright/test')
+const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    // Reset DB and create user
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
     await page.goto('http://localhost:5173')
   })
 
@@ -9,5 +18,26 @@ describe('Blog app', () => {
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
     await expect(page.getByRole('button', { name: "login" })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('logs in with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('salainen')
+      await page.getByRole('button', { name: "login" }).click()
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('wrong')
+      await page.getByRole('button', { name: "login" }).click()
+
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('Wrong username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+    })
   })
 })
